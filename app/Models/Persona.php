4ca3a07f61914350eaeda46412f3bfbfdb7db025
Eduard <eduard@localhost.localdomain>
2022-06-13 01:09:16 -0400

    update query

--- app/Models/Persona.php
+++ app/Models/Persona.php
@@ -1,9 +1,13 @@
 <?php
 namespace App\Models;
+use App\Models\TipoDocumento;
 use Illuminate\Database\Eloquent\Model;
 class Persona extends Model{
     protected $table="persona";
     public $timestamps=false;
     protected $fillable = ['ID','Nombre','Paterno','Materno','Correo','Telefono','CI','Nacimineto','TipoDocumento','Direccion','Referencia','FechaCreado','FechaActualizado','FechaEliminado'];
+    public function tipodocumento() {
+        return $this->hasOne(TipoDocumento::class,'ID','TipoDocumento');
+    }
 }
 ?>
