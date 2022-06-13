0c84705139f5a3adf44c19dd67b74865a755545c
Eduard <eduard@localhost.localdomain>
2022-06-13 00:38:09 -0400

    update

--- graphql/types.php
+++ graphql/types.php
@@ -11,4 +11,21 @@ $validacionLoginType=new ObjectType([
     ]
 ]);
 
+$UsuarioType=new ObjectType([
+    'name'=>'Objeto Usuario',
+    'description'=>'Tabla Usuario',
+    'fields'=>[
+        'ID'=>Type::int(),
+        'Usuario'=>Type::string(),
+        'Pwd'=>Type::string(),
+        'Persona'=>Type::int(),
+        'Rango'=>Type::int(),
+        'Perfil'=>Type::int(),
+        'Poli'=>Type::int(),
+        'FechaCreado'=>Type::string(),
+        'FechaActualizado'=>Type::string(),
+        'FechaEliminado'=>Type::string(),
+    ]
+]);
+
 ?>
