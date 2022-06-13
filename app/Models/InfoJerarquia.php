1a7d0d21b86a325d83401822de82c1f63dbc2ca4
Alejandro Loayza <loayzaclaros@gmail.com>
2022-06-12 21:13:17 -0400

    modelo corregido

--- app/Models/InfoJerarquia.php
+++ app/Models/InfoJerarquia.php
@@ -4,6 +4,6 @@ use Illuminate\Database\Eloquent\Model;
 class InfoJerarquia extends Model{
     protected $table="info_jerarquia";
     public $timestamps=false;
-    protected $fillable = ['ID','Departamento','FechaCreado','FechaActualizado','FechaEliminado'];
+    protected $fillable = ['ID','Rango','FechaCreado','FechaActualizado','FechaEliminado'];
 }
 ?>
--- app/Models/Persmisos.php
+++ app/Models/Persmisos.php
@@ -4,6 +4,6 @@ use Illuminate\Database\Eloquent\Model;
 class Permisos extends Model{
     protected $table="permisos";
     public $timestamps=false;
-    protected $fillable = ['ID','Cod1','Cod2','Cod3','Cod4','Cod5','Cod6','Cod7','Cod18','Cod9','Cod10','FechaCreado','FechaActualizado','FechaEliminado'];
+    protected $fillable = ['ID','Cod1','Cod2','Cod3','Cod4','Cod5','Cod6','Cod7','Cod8','Cod9','Cod10','FechaCreado','FechaActualizado','FechaEliminado'];
 }
 ?>
