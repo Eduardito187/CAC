43aa5392d3d2e96cc9647530265ee4d5b5fd6971
Eduard <eduard@localhost.localdomain>
2022-07-03 00:07:41 -0400

    login

--- graphql/types.php
+++ graphql/types.php
@@ -3,6 +3,15 @@ use App\Models\Usuario;
 use GraphQL\Type\Definition\ObjectType;
 use GraphQL\Type\Definition\Type;
 
+$validacionLoginType=new ObjectType([
+    'name' => 'Validacion de Login',
+    'description' => 'Se valida el inicio al sistema',
+    'fields'=>[
+        'estado'=>Type::boolean(),
+        'id_cuenta'=>Type::int()
+    ]
+]);
+
 $UsuarioType=new ObjectType([
     'name'=>'Objeto Usuario',
     'description'=>'Tabla Usuario',
