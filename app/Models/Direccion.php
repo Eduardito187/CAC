019e3cf66ebffddc519bba6df6426c8e23bddf08
Eduard <eduard@localhost.localdomain>
2022-07-04 22:58:27 -0400

    error solucion

--- app/Models/Direccion.php
+++ app/Models/Direccion.php
@@ -4,6 +4,6 @@ use Illuminate\Database\Eloquent\Model;
 class Direccion extends Model{
     protected $table="direccion";
     public $timestamps=false;
-    protected $fillable = ['ID','Zona','Barrio','Calle','Casa','Geo','Municipio','Distrito','Uv','Canton''FechaCreado','FechaActualizado','FechaEliminado'];
+    protected $fillable = ['ID','Zona','Barrio','Calle','Casa','Geo','Municipio','Distrito','Uv','Canton','FechaCreado','FechaActualizado','FechaEliminado'];
 }
 ?>
