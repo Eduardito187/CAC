46fc8c2a97157dbbbda60a346c234590bab04fec
Eduard <eduard@localhost.localdomain>
2022-07-10 21:04:47 -0400

    ok

--- app/Models/Propietario.php
+++ app/Models/Propietario.php
@@ -4,6 +4,6 @@ use Illuminate\Database\Eloquent\Model;
 class Propietario extends Model{
     protected $table="propietario";
     public $timestamps=false;
-    protected $fillable = ['ID','Nombre','Apellido','CI','Telefono','FechaCreado','FechaActualizado','FechaEliminado'];
+    protected $fillable = ['ID','Nombre','Apellido','CI','Telefono','Direccion','FechaCreado','FechaActualizado','FechaEliminado'];
 }
 ?>
