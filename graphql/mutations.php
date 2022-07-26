c28de7e90806a0822f25ef0ec47b8777de2e7d24
WaneySist <loayzaclaros@gmail.com>
2022-07-26 04:17:34 -0400

    cambios mutations

--- graphql/mutations.php
+++ graphql/mutations.php
@@ -4,10 +4,12 @@ use GraphQL\Type\Definition\Type;
 
 require('mutations/Login.php');
 require('mutations/Permisos.php');
+require('mutations/Usuario.php');
 
 $mutations=array();
 $mutations+=$Login;
 $mutations+=$Permisos;
+$mutations+=$Usuario;
 $rootMutation=new ObjectType([
     'name'=>'Mutation',
     'fields' => $mutations
